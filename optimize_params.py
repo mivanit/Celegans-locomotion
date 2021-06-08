3df160cde15ff54c7f25d784b4c77c4a295d28ad
mivanit <mivanits@umich.edu>
2021-06-08 02:23:20 -0400

    playing with params for genetic algo

--- optimize_params.py
+++ optimize_params.py
@@ -823,7 +823,7 @@ def compute_gen_sizes(
 	return output
 
 
-MODPARAMS_DEFAULT_RANGES : ModParamsRanges = {
+MODPARAMS_DEFAULT_RANGES_CHEMO_V6 : ModParamsRanges = {
 	ModParam("conn",   "Head,AWA,RIM,chem") : RangeTuple(-40000,40000),
 	ModParam("conn",   "Head,RIM,RMD*,chem") : RangeTuple(-1000,1000),
 	ModParam("params", "ChemoReceptors.kappa") : RangeTuple(0.0, 1000.0),
@@ -836,10 +836,34 @@ MODPARAMS_DEFAULT_RANGES : ModParamsRanges = {
 	# ModParam("conn",   "") : RangeTuple(,),
 }
 
+
+# desmos reference
+# https://www.desmos.com/calculator/wqyexesoml
+# better one:
+# https://www.desmos.com/calculator/c4rr96zrxc
+MODPARAMS_DEFAULT_RANGES_CHEMO_V7 : ModParamsRanges = {
+	# conns
+	ModParam("conn",   "Head,AWA,AIY,chem") : RangeTuple(-20.0, 20.0),
+	ModParam("conn",   "Head,AIY,RIA,chem") : RangeTuple(-600.0, 100.0),
+	ModParam("conn",   "Head,RIA,RMD*,chem") : RangeTuple(-100.0,100.0),
+	# chemo params
+	ModParam("params", "ChemoReceptors.kappa") : RangeTuple(150.0, 300.0),
+	ModParam("params", "ChemoReceptors.lambda") : RangeTuple(-50000, -10000.0),
+	# neuron params
+	ModParam("params", "Head.neurons.AWA.tau") : RangeTuple(0.005, 0.01),
+	ModParam("params", "Head.neurons.AWA.theta") : RangeTuple(-1.0, 1.0),
+	ModParam("params", "Head.neurons.AIY.tau") : RangeTuple(0.005, 0.01),
+	ModParam("params", "Head.neurons.AIY.theta") : RangeTuple(0.0, 5.0),
+	ModParam("params", "Head.neurons.RIA.tau") : RangeTuple(0.005, 0.01),
+	ModParam("params", "Head.neurons.RIA.theta") : RangeTuple(0.0, 1.0),
+	# ModParam("params", "") : RangeTuple(,),
+	# ModParam("conn",   "") : RangeTuple(,),
+}
+
 def run_genetic_algorithm(
 		# for setup
 		rootdir : Path = "data/geno_sweep/",
-		ranges : ModParamsRanges = MODPARAMS_DEFAULT_RANGES,
+		ranges : ModParamsRanges = MODPARAMS_DEFAULT_RANGES_CHEMO_V7,
 		first_gen_size : int = 100,
 		gen_count : int = 20,
 		factor_cull : float = 0.49,
