0a175d49815c2edf26a17244a8cf2dbd9fde5a24
mivanit <mivanits@umich.edu>
2021-06-08 02:54:34 -0400

    more playing with params

--- optimize_params.py
+++ optimize_params.py
@@ -25,7 +25,7 @@ else:
 from pyutil.util import (
 	ModParam, ModTypes, Path,mkdir,joinPath,
 	strList_to_dict,ParamsDict,ModParamsDict,ModParamsRanges,
-	RangeTuple, # MODPARAMS_DEFAULT_RANGES,
+	RangeTuple,
 	VecXY,dump_state,
 	find_conn_idx,find_conn_idx_regex,
 	genCmd_singlerun,
@@ -34,6 +34,8 @@ from pyutil.util import (
 	read_body_data,CoordsRotArr,
 	prntmsg,
 )
+
+from pyutil.modparams_ranges import DEFAULT_RANGES
 
 Process = Any
 Population = List[ModParamsDict]
@@ -823,55 +825,20 @@ def compute_gen_sizes(
 	return output
 
 
-MODPARAMS_DEFAULT_RANGES_CHEMO_V6 : ModParamsRanges = {
-	ModParam("conn",   "Head,AWA,RIM,chem") : RangeTuple(-40000,40000),
-	ModParam("conn",   "Head,RIM,RMD*,chem") : RangeTuple(-1000,1000),
-	ModParam("params", "ChemoReceptors.kappa") : RangeTuple(0.0, 1000.0),
-	ModParam("params", "ChemoReceptors.lambda") : RangeTuple(-100000, -1000000),
-	ModParam("params", "Head.neurons.AWA.tau") : RangeTuple(0.0, 0.5),
-	ModParam("params", "Head.neurons.AWA.theta") : RangeTuple(-10.0, 10.0),
-	ModParam("params", "Head.neurons.RIM.tau") : RangeTuple(0.0, 0.5),
-	ModParam("params", "Head.neurons.RIM.theta") : RangeTuple(-10.0, 10.0),
-	# ModParam("params", "") : RangeTuple(,),
-	# ModParam("conn",   "") : RangeTuple(,),
-}
 
-
-# desmos reference
-# https://www.desmos.com/calculator/wqyexesoml
-# better one:
-# https://www.desmos.com/calculator/c4rr96zrxc
-MODPARAMS_DEFAULT_RANGES_CHEMO_V7 : ModParamsRanges = {
-	# conns
-	ModParam("conn",   "Head,AWA,AIY,chem") : RangeTuple(-20.0, 20.0),
-	ModParam("conn",   "Head,AIY,RIA,chem") : RangeTuple(-600.0, 100.0),
-	ModParam("conn",   "Head,RIA,RMD*,chem") : RangeTuple(-100.0,100.0),
-	# chemo params
-	ModParam("params", "ChemoReceptors.kappa") : RangeTuple(150.0, 300.0),
-	ModParam("params", "ChemoReceptors.lambda") : RangeTuple(-50000, -10000.0),
-	# neuron params
-	ModParam("params", "Head.neurons.AWA.tau") : RangeTuple(0.005, 0.01),
-	ModParam("params", "Head.neurons.AWA.theta") : RangeTuple(-1.0, 1.0),
-	ModParam("params", "Head.neurons.AIY.tau") : RangeTuple(0.005, 0.01),
-	ModParam("params", "Head.neurons.AIY.theta") : RangeTuple(0.0, 5.0),
-	ModParam("params", "Head.neurons.RIA.tau") : RangeTuple(0.005, 0.01),
-	ModParam("params", "Head.neurons.RIA.theta") : RangeTuple(0.0, 1.0),
-	# ModParam("params", "") : RangeTuple(,),
-	# ModParam("conn",   "") : RangeTuple(,),
-}
 
 def run_genetic_algorithm(
 		# for setup
 		rootdir : Path = "data/geno_sweep/",
-		ranges : ModParamsRanges = MODPARAMS_DEFAULT_RANGES_CHEMO_V7,
-		first_gen_size : int = 100,
-		gen_count : int = 20,
-		factor_cull : float = 0.49,
-		factor_repro : float = 2.0,
+		ranges : ModParamsRanges = DEFAULT_RANGES,
+		first_gen_size : int = 24,
+		gen_count : int = 5,
+		factor_cull : float = 0.8,
+		factor_repro : float = 1.25,
 		# passed to `run_generation`
-		params_base : ParamsDict = load_params("input/chemo_v6.json"),
-		sigma : float = 0.05,
-		mutprob : float = 0.05,
+		params_base : ParamsDict = load_params("input/chemo_v7.json"),
+		sigma : float = 2.0,
+		mutprob : float = 0.1,
 		func_extract : ExtractorFunc = extract_food_dist_inv,
 		gene_combine : GenoCombineFunc = combine_geno_select,
 		gene_combine_kwargs : Dict[str,Any] = dict(),
