8519594d9460cce7665423673f5f5246e1c207c8
mivanit <mivanits@umich.edu>
2021-09-01 16:22:20 -0600

    misc, BROKENgit add .

--- pyutil/diffusion/particlesim.py
+++ pyutil/diffusion/particlesim.py
@@ -6,7 +6,9 @@ from nptyping import NDArray
 import matplotlib.pyplot as plt
 
 import numba
-from numpy.lib.npyio import save
+
+import cppyy
+cppyy.include('Collide_standalone.h')
 
 ParticlePositions = NDArray[(Any, 2), float]
 
@@ -25,13 +27,16 @@ def initialize_particles(pos : Tuple[float,float], n : int) -> ParticlePositions
 	nd_pos : NDArray[2, float] = np.array(pos, dtype=np.float64)
 	return np.full((n,2), nd_pos)
 
+@numba.njit(cache=True)
+def dummy_func(positions : NDArray):
+	return positions * 5.0
 
 @numba.njit(cache=True)
 def iterate_particles(
-		positions : ParticlePositions,
+		positions : NDArray,
 		mean_dist : float,
 		p_static : float,
-	) -> ParticlePositions:
+	) -> NDArray:
 	"""iterate the particle positions through a random walk
 	
 	### Parameters:
@@ -63,12 +68,87 @@ def iterate_particles(
 
 	thetas : NDArray[n_particles, float] = np.random.uniform(0, 2*np.pi, size = n_particles)
 	# convert to cartesian coordinates
-	pos_delta : ParticlePositions = np.stack(
+	pos_delta : NDArray = np.stack(
 		(dists*np.cos(thetas), dists*np.sin(thetas)),
 		axis = 1,
 	)
 
 	return positions + pos_delta
+
+
+@numba.njit(cache=True)
+def run_particlesim(
+		positions : NDArray,
+		n_particles : int,
+		mean_dist : float,
+		p_static : float, 
+		n_iterations : NDArray,
+		save_every : int,
+	) -> NDArray:
+
+	output : NDArray = np.empty(
+		(
+			n_iterations // save_every,
+			n_particles,
+			2,
+		), # shape
+		np.int32,
+	)
+
+	for i in range(n_iterations):
+		# positions = iterate_particles(
+		# 	positions = positions,
+		# 	mean_dist = mean_dist,
+		# 	p_static = p_static,
+		# )
+		positions = dummy_func(
+			positions,
+		)
+		
+		if i % save_every == 0:
+			output[i // save_every] = positions
+	
+	return output
+
+
+
+@numba.njit(cache=True)
+def run_particlesim_inline(
+		positions : NDArray,
+		n_particles : int,
+		mean_dist : float,
+		p_static : float, 
+		n_iterations : NDArray,
+		save_every : int,
+	) -> NDArray:
+
+	output : NDArray = np.empty(
+		(
+			n_iterations // save_every,
+			n_particles,
+			2,
+		), # shape
+		np.int32,
+	)
+
+	for i in range(n_iterations):
+		dists : NDArray[n_particles, float] = np.random.poisson(lam = mean_dist, size = n_particles)		
+		static : NDArray[n_particles, float] = np.random.rand(n_particles)
+		dists = dists * (static > p_static)
+		thetas : NDArray[n_particles, float] = np.random.uniform(0, 2*np.pi, size = n_particles)
+		pos_delta : NDArray = np.stack(
+			(dists*np.cos(thetas), dists*np.sin(thetas)),
+			axis = 1,
+		)
+
+		# positions = positions + pos_delta
+		
+		if i % save_every == 0:
+			output[i // save_every] = positions
+	
+	return output
+
+
 
 
 def positions_to_heatmap(
@@ -95,37 +175,6 @@ def positions_to_heatmap(
 	return H
 
 
-@numba.njit(cache=True)
-def run_particlesim(
-		n_particles : int = 1000000,
-		mean_dist : float = 1.0,
-		p_static : float = 0.2, 
-		initial_pos : Tuple[float,float] = (0,0),
-		n_iterations : int = 1000,
-		save_every : int = 50,
-	) -> NDArray:
-
-	# initial positions
-	positions : ParticlePositions = np.full((n_particles,2), np.array(initial_pos))
-
-	output : NDArray = np.empty((
-		n_iterations // save_every,
-		n_particles,
-		2,
-	))
-
-	for i in range(n_iterations):
-		positions = iterate_particles(
-			positions = positions,
-			mean_dist = mean_dist,
-			p_static = p_static,
-		)
-
-		if i % save_every == 0:
-			output[i // save_every] = positions
-	
-	return positions
-
 
 def run_particlesim_wrapper(
 		n_particles : int = 1000000,
@@ -136,11 +185,18 @@ def run_particlesim_wrapper(
 		save_every : int = 50,
 	):
 
-	data = run_particlesim(
+	# initial positions
+	positions : ParticlePositions = np.full(
+		(n_particles, 2), 
+		fill_value = initial_pos,
+		dtype = np.float32,
+	)
+
+	data = run_particlesim_inline(
+		positions,
 		n_particles,
 		mean_dist,
 		p_static,
-		initial_pos,
 		n_iterations,
 		save_every,
 	)
@@ -150,6 +206,40 @@ def run_particlesim_wrapper(
 		plt.show()
 
 
+if __name__ == '__main__':
+	import fire
+	fire.Fire(run_particlesim_wrapper)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 def something_copilot_made(positions, velocities, dt, d, D, gamma, kT,
 							   friction):
 	"""
@@ -199,7 +289,3 @@ def something_copilot_made(positions, velocities, dt, d, D, gamma, kT,
 		)
 	return new_positions, new_velocities
 
-
-if __name__ == '__main__':
-	import fire
-	fire.Fire(run_particlesim_wrapper)
